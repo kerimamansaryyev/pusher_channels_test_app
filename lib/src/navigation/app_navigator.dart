3b31fcc7e8cc2e05bd7922ff333bf6b7e6ed30e5
kerimamansaryyev <keamansaryyev@gmail.com>
2023-08-19 13:41:24 -0500

    Set up the DI

---
+++ lib/src/navigation/app_navigator.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class AppNavigator {
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
+  const AppNavigator();
+
+  Future<R?> push<R>(
+    Route<R> route, {
+    BuildContext? context,
+    bool useRoot = false,
+  }) async {
+    return _navigator(context, useRoot: useRoot).push(route);
+  }
+
+  Future<R?> pushReplacement<R>(
+    Route<R> route, {
+    BuildContext? context,
+    bool useRoot = false,
+  }) async {
+    return _navigator(context, useRoot: useRoot).pushReplacement(route);
+  }
+
+  void popUntilRoot(BuildContext context) =>
+      _navigator(context).popUntil((route) => route.isFirst);
+
+  void pop({
+    BuildContext? context,
+  }) =>
+      _navigator(
+        context,
+      );
+
+  static NavigatorState _navigator(
+    BuildContext? context, {
+    bool useRoot = false,
+  }) =>
+      (context == null || useRoot)
+          ? navigatorKey.currentState!
+          : Navigator.of(context);
+}
---
+++ lib/src/navigation/per_page_navigator_mixin.dart
@@ -0,0 +1,5 @@
+import 'package:pusher_channels_test_app/src/navigation/app_navigator.dart';
+
+mixin PerPageNavigatorMixin {
+  AppNavigator get appNavigator;
+}
